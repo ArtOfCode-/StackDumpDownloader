5a0f5b9ea25b1f580c27a59682b36a55b48c4118
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-02-25 18:10:41 +0000

    add example config

---
+++ config-example.py
@@ -0,0 +1,13 @@
+class Config:
+    Database = {
+        "host": "",
+        "user": "",
+        "pass": "",
+        "name": ""
+    }
+
+    General = {
+        'site_whitelist': [],
+        'site_blacklist': [],
+        'site_mode': 'whitelist'
+    }
